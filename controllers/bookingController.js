b58d45a991b8449fa2d8748548263a3c2d0ff2f5
Abdelrahman435 <ah0383723@gmail.com>
2024-06-29 00:51:50 +0300

    update

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -10,6 +10,7 @@ const Cart = require("../models/cartModel");
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the booked product
   const products = req.body.products; // Array of products from the request body
+  const allProductIds = products.map((product) => product.id).join(",");
 
   const lineItems = products.map((product) => ({
     price_data: {
@@ -18,7 +19,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         name: `${product.name} Product`,
         images: [product.image], // Assuming images are an array in each product
         metadata: {
-          productId: product.id, // Example of adding metadata
+          productsIDs: allProductIds, // Example of adding metadata
         },
       },
       unit_amount: product.price * 100, // Assuming product price is in dollars
@@ -47,26 +48,26 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   try {
     const product = session.client_reference_id;
-    const user = (await User.findOne({ email: session.customer_email })).id;
+    const user = await User.findOne({ email: session.customer_email });
     const price = session.amount_total / 100;
     await Bookings.create({ user, product, price });
     await User.findByIdAndUpdate(
-      user,
-      { $push: { bookedProducts: product } }, //$push to add to the array
+      user.id,
+      { $push: { bookedProducts: { $each: user.cartProducts } } }, //$push to add to the array
       {
         new: true,
         runValidators: true,
       }
     );
     await User.findByIdAndUpdate(
-      user,
-      { $pull: { cartProducts: product } }, //$push to add to the array
+      user.id,
+      { $set: { cartProducts: [] }}, //$push to add to the array
       {
         new: true,
         runValidators: true,
       }
     );
-    await Cart.findOneAndDelete({ user: user, product: product });
+    await Cart.deleteMany({ user: user.id });
   } catch (err) {
     console.error("Error creating booking:", err);
   }
