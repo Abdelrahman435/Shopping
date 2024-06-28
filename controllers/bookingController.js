149418f44967090859fc1035370d8793c1b4ff48
Abdelrahman435 <ah0383723@gmail.com>
2024-06-28 23:26:08 +0300

    any

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -10,7 +10,6 @@ const Cart = require("../models/cartModel");
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the booked product
   const products = req.body.products; // Array of products from the request body
-  const allProductIds = products.map((product) => product.id).join(",");
 
   const lineItems = products.map((product) => ({
     price_data: {
@@ -20,7 +19,6 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         images: [product.image], // Assuming images are an array in each product
         metadata: {
           productId: product.id, // Example of adding metadata
-          allProductIds: allProductIds, // Adding all product IDs
         },
       },
       unit_amount: product.price * 100, // Assuming product price is in dollars
@@ -40,6 +38,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     line_items: lineItems,
   });
 
+  console.log(lineItems[0].price_data.product_data.metadata);
+
   res.status(200).json({
     status: "success",
     session,
@@ -47,40 +47,36 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 });
 
 const createBookingCheckout = async (session) => {
-  const user = (await User.findOne({ email: session.customer_email })).id;
-  const allProductIds =
-    session.line_items[0].price_data.product_data.metadata.allProductIds;
-  await User.findByIdAndUpdate(
-    user,
-    {
-      $push: {
-        bookedProducts: { $each: allProductIds },
-      },
-    }, //$push to add to the array
-    {
-      new: true,
-      runValidators: true,
-    }
-  );
-  await User.findByIdAndUpdate(
-    user,
-    {
-      $pull: {
-        cartProducts: { $each: allProductIds },
-      },
-    }, //$push to add to the array
-    {
-      new: true,
-      runValidators: true,
-    }
-  );
-  await Cart.findOneAndDelete({ user: user });
-  return user;
+  try {
+    const product = session.client_reference_id;
+    const user = (await User.findOne({ email: session.customer_email })).id;
+    const price = session.amount_total / 100;
+    await Bookings.create({ user, product, price });
+    await User.findByIdAndUpdate(
+      user,
+      { $push: { bookedProducts: product } }, //$push to add to the array
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    await User.findByIdAndUpdate(
+      user,
+      { $pull: { cartProducts: product } }, //$push to add to the array
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    await Cart.findOneAndDelete({ user: user, product: product });
+  } catch (err) {
+    console.error("Error creating booking:", err);
+  }
 };
 
 exports.webhookCheckout = catchAsync(async (req, res, next) => {
   const signature = req.headers["stripe-signature"];
-  let flag 
+
   let event;
   try {
     event = stripe.webhooks.constructEvent(
@@ -94,8 +90,8 @@ exports.webhookCheckout = catchAsync(async (req, res, next) => {
   }
 
   if (event.type === "checkout.session.completed") {
-    flag = await createBookingCheckout(event.data.object);
+    await createBookingCheckout(event.data.object);
   }
 
-  res.status(200).json({ received: true, flag });
+  res.status(200).json({ received: true });
 });
