ceb0ec416bf7378ecac0f6025249770b1f37c540
Abdelrahman435 <ah0383723@gmail.com>
2024-06-29 00:41:30 +0300

    update

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -8,10 +8,8 @@ const User = require("../models/userModel");
 const Cart = require("../models/cartModel");
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  // 1) Get the booked product
   const products = req.body.products; // Array of products from the request body
-
-  // Collect all product IDs as an array
-  const allProductIds = products.map(product => product.id).join(',');
 
   const lineItems = products.map((product) => ({
     price_data: {
@@ -20,13 +18,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         name: `${product.name} Product`,
         images: [product.image], // Assuming images are an array in each product
         metadata: {
-          productId: product.id, // Example of adding individual product ID
-          allProductIds: allProductIds, // Adding all product IDs as an array
+          productId: product.id, // Example of adding metadata
         },
       },
       unit_amount: product.price * 100, // Assuming product price is in dollars
     },
-    quantity: 1 // Assuming quantity is provided in each product
+    quantity: 1, // Assuming quantity is provided in each product
   }));
 
   const session = await stripe.checkout.sessions.create({
@@ -52,37 +49,24 @@ const createBookingCheckout = async (session) => {
     const product = session.client_reference_id;
     const user = (await User.findOne({ email: session.customer_email })).id;
     const price = session.amount_total / 100;
-
-    const allProductIds =
-      session.line_items[0].price_data.product_data.metadata.allProductIds;
-      await Cart.deleteMany({ user: user });
-
+    await Bookings.create({ user, product, price });
     await User.findByIdAndUpdate(
       user,
-      {
-        $push: {
-          bookedProducts: { $each: allProductIds },
-        },
-      },
+      { $push: { bookedProducts: product } }, //$push to add to the array
       {
         new: true,
         runValidators: true,
       }
     );
-
     await User.findByIdAndUpdate(
       user,
-      {
-        $pull: {
-          cartProducts: { $each: allProductIds },
-        },
-      },
+      { $pull: { cartProducts: product } }, //$push to add to the array
       {
         new: true,
         runValidators: true,
       }
     );
-
+    await Cart.findOneAndDelete({ user: user, product: product });
   } catch (err) {
     console.error("Error creating booking:", err);
   }
