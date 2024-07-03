0447ca3394079a3108513ccb23a9f313a854e83e
Abdelrahman435 <ah0383723@gmail.com>
2024-07-03 04:22:41 +0300

    photo

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -9,17 +9,17 @@ const cloudinary = require("../utils/cloudinary");
 
 const multerStorage = multer.memoryStorage();
 
-const multerFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith("image")) {
-    cb(null, true);
-  } else {
-    cb(new AppError("Not an image! Please upload only images.", 400), false);
-  }
-};
+// const multerFilter = (req, file, cb) => {
+//   if (file.mimetype.startsWith("image")) {
+//     cb(null, true);
+//   } else {
+//     cb(new AppError("Not an image! Please upload only images.", 400), false);
+//   }
+// };
 
 const upload = multer({
   storage: multerStorage,
-  fileFilter: multerFilter,
+  // fileFilter: multerFilter,
 });
 
 exports.uploadUserPhoto = upload.single("photo");
